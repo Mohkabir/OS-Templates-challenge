6390aa8940b3df670893d7d57e5de586bd280364
Kabir <60716161+Mohkabir@users.noreply.github.com>
2020-10-07 09:29:25 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuimg = document.querySelector('#menuBar');
+
+let nav = document.querySelector('#navigation');
+
+menuimg.addEventListener('click',function(){
+  nav.classList.toggle('show')
+})
+
+
