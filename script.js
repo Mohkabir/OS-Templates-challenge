adaf1add3a0a01bbf5777a358e9b9a4befbfea72
Kabir <60716161+Mohkabir@users.noreply.github.com>
2020-11-21 09:57:08 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
   $('#menuBar').click(function(){
-    $('#navigation').slideToggle(1000);
+    $('#navigation').slideToggle(500);
   });
 
-  $('#top').click(function(){
+
+  $('#top, #navigation a').click(function(){
+
     var userLocation = window.location.href;
-  var currentUrl = userLocation.substring(userLocation.lastIndexOf('#')+1);
+    var currentUrl = userLocation.substring(userLocation.lastIndexOf('#')+1);
 
   if(currentUrl == 'home'){
     $('html, body').animate({
       scrollTop: $('#scroll-' + currentUrl).offset().top
     },1000);
+  }else if(currentUrl == 'homepage'){
+    $('html, body').animate({
+      scrollTop: $('#scroll-' + currentUrl).offset().top
+    },1000);
+  }else if(currentUrl == 'aboutUs'){
+    $('html, body').animate({
+      scrollTop: $('#scroll-' + currentUrl).offset().top
+    },1000);
   }
   })
+  
+
+    
+
   
 });
 
